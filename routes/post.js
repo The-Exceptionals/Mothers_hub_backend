a3619df58e1be886b86ae7cab76c0ae88795567c
melica101 <amalicamelly@gmail.com>
2021-02-26 21:20:26 +0300

    get all added for posts

--- routes/post.js
+++ routes/post.js
@@ -60,6 +60,13 @@ router.route("/getOwnBlog").get(middleware.checkToken, (req, res) => {
   });
 });
 
+router.route("/getAll").get(middleware.checkToken, (req, res) => {
+  Post.find({}, (err, result) => {
+    if (err) return res.json(err);
+    return res.json({ data: result });
+  });
+});
+
 router.route("/getOtherBlog").get(middleware.checkToken, (req, res) => {
   Post.find({ username: { $ne: req.decoded.username } }, (err, result) => {
     if (err) return res.json(err);
