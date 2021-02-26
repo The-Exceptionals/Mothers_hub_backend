0f996d153c4e402dc6fd141fb33e96efc6270442
melica101 <amalicamelly@gmail.com>
2021-02-26 22:06:57 +0300

    make changes

--- index.js
+++ index.js
@@ -38,7 +38,11 @@ data = {
     "Support me",
 };
 
-console.log(listEndpoints(app));
+app._router.stack.forEach(function(r){
+  if (r.route && r.route.path){
+    console.log(r.route.path)
+  }
+})
 
 app.route("/").get((req, res) => res.json(data));
 
